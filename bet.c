dcd5ebe582b955417720e91eef292e614426c27e
Dennis Yegon <kipkemoidennisyegon@gmail.com>
2023-05-14 06:18:56 -0700

    1. Simple shell 0.1

---
+++ bet.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"shell.h"
+int main(void)
+{
+	printf("this project is all about she
+}
---
+++ interpret.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+#define CMD_MAX 150
+/*
+ * main ()-The main function of the interpret program.
+ * @CMD_MAX: command line charactors
+ * @void:no input parameter
+ * @cmd_status:Exit status of the program
+ * Return (0): always return succes
+ */
+int main(void)
+{
+char cmd[CMD_MAX];
+char full_path[CMD_MAX];
+char cmd_status;
+while (1)
+{
+printf("enter your command");
+fgets(cmd, CMD_MAX, stdin);
+if (feof(stdin))
+{
+exit(0);
+}
+cmd[strcspn(cmd, "\n")] = '\0';
+strcat(full_path, cmd);
+cmd_status = system(full_path);
+if (cmd_status == -1)
+{
+printf("command is not found");
+}
+}
+return (0);
+}
