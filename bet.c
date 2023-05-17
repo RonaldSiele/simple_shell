c4fb89423a97a21bf6d0f9f0d69ff753dcb7c013
Dennis Yegon <kipkemoidennisyegon@gmail.com>
2023-05-17 00:08:44 -0700

    2 simple shell

--- bet.c
+++ bet.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include"shell.h"
+/*
+ *main- entry point of the code
+ *return (0):Always return success.
+ */
 int main(void)
 {
-	printf("this project is all about she
+printf("this project is all about shell");
+return (0);
 }
---
+++ interpret1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+#define CMD_NUMBER 100
+int main(int argc, char *argv[1])
+{
+char cmd[CMD_NUMBER];
+char whole_path[CMD_NUMBER];
+char command_status;
+if (argc < 2)
+{
+printf("usage:%s command\n", argv[1]);
+exit(1);
+
+}
+while (1)
+{
+printf("enter_promt");
+fgets(cmd, CMD_NUMBER, stdin);
+if (feof(stdin))
+{
+exit(0);
+}
+cmd[strcspn(cmd, "\n")] = '\0';
+strcpy(whole_path, argv[1]);
+strcat(whole_path, "");
+strcat(whole_path, cmd);
+
+command_status = system(whole_path);
+
+if (command_status == -1)
+{
+printf("command not found");
+}
+}
+return (0);
+
+
+
+
+}
