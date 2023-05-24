e9743d44bca2406915f67972ff540426d0fd58e2
Dennis Yegon <kipkemoidennisyegon@gmail.com>
2023-05-24 07:42:21 -0700

    mycd.c

--- mycd.c
+++ mycd.c
@@ -8,14 +8,15 @@
 int MYCD(int argc, char *argv[])
 {
 char newDir[MYCD_MAX];
-if (argc == 1 || (argc ==2 && strcmp(argv[2],"-") == 0))
+if (argc == 1 || (argc == 2 && strcmp(argv[2], "-") == 0))
 {
 const char* homeDir = getenv("HOME");
 if (homeDir != NULL)
 {
 chdir(homeDir);
-setenv("PWD",homeDir,1);
-}else
+setenv("PWD", homeDir, 1);
+}
+else
 {
 printf("cd : No $HOME environment variable found.\n");
 }
@@ -25,12 +26,14 @@ else if (argc == 2)
 if (chdir(argv[1]) == 0)
 {
 getcwd(newDir, sizeof(newDir));
-setenv("PWD",newDir, 1);
-}else
-{
-printf("cd: %s: no such directory. \n",argv[1]);
+setenv("PWD", newDir, 1);
 }
-}else 
+else
+{
+printf("cd: %s: no such directory. \n", argv[1]);
+}
+}
+else
 {
 printf("cd: Too many arguments .\n");
 }
