c5cb0d0c16e5a7df49b2e594fd5394fcae9fa5c1
Dennis Yegon <kipkemoidennisyegon@gmail.com>
2023-05-24 03:14:40 -0700

    bet.c

--- bet.c
+++ bet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main1 - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main1(void)
+int main(void)
 {
 printf("simple shell project\n");
 return (0);
