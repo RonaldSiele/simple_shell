ab641159d819d9f9d0df11f6ef6da8d827b4c6cd
root <kimutaironald98@gmail.com>
2023-05-24 07:01:21 -0700

    getline.c

--- getline.c
+++ getline.c
@@ -4,6 +4,11 @@
 #include<unistd.h>
 
 #define GETLINE_MAX 100
+/**
+ * getline_builtin - Reads a line from stdin using getline.
+ *
+ * Return: The read line or NULL on failure or end of input.
+ */
 char *getline_builtin()
 {
 static char buffer[GETLINE_MAX];
@@ -21,21 +26,26 @@ line = NULL;
 line_size = 0;
 }
 read = getline(&line, &line_size, stdin);
-if(read < 0)
+if (read < 0)
 {
-return NULL;
+return (NULL);
 }
 if (read > 0 && line[read - 1] == '\n')
 {
 line[read - 1] = '\0';
 }
 strncpy(buffer, line, GETLINE_MAX);
-return buffer;
+return (buffer);
 }
-int GETLINE()
+/**
+ * GETLINE - Reads lines from stdin getline_builtin untill NULL is returned.
+ *
+ * Return: 0 on success.
+ */
+int GETLINE(void)
 {
 char *line;
-while(1)
+while (1)
 {
 line = getline_builtin();
 if (line == NULL)
@@ -43,7 +53,7 @@ if (line == NULL)
 printf("Goodbye!\n");
 break;
 }
-printf("You entered:%s\n",line);
+printf("You entered:%s\n", line);
 }
 return (0);
 }
