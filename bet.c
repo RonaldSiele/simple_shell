e331f8a0efda682f9a48d7e79c9d83774dcdf57e
Dennis Yegon <kipkemoidennisyegon@gmail.com>
2023-05-24 01:29:44 -0700

    betty would be proud

--- bet.c
+++ bet.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-/*
- *main- entry point of the program
- *@void - return nothing
- *Return:Always return success 0.
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("this project is all about shell");
-return (0);
+	return (0);
 }
